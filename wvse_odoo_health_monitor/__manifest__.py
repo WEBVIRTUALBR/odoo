83f5b7bc3f8fb361777fa95da1b938cc63117ee9
Rafael Sabino <rafael.sabino@hotmail.com.br>
2026-04-27 01:52:57 -0300

    Update version 19.0

--- wvse_odoo_health_monitor/__manifest__.py
+++ wvse_odoo_health_monitor/__manifest__.py
@@ -187,8 +187,8 @@ Technical Notes
     # Internals images.
     'images': [
         #'static/description/main_screen.gif',
-        'static/description/wvse_odoo_health_monitor_cover.jpg',
-        'static/description/wvse_odoo_health_monitor_screenshot.jpg',
+        'static/description/image_cover.jpg',
+        'static/description/image_screenshot.jpg',
         'static/description/*.png',
         'static/description/*.jpg',
         'static/src/img/*.png',
